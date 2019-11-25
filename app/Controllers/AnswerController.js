04a09f500c287e5655404986ba72e5187edf54f3
JD-Fuller <jdfuller@live.com>
2019-11-25 16:59:07 -0700

    hooked up

--- app/Controllers/AnswerController.js
+++ app/Controllers/AnswerController.js
@@ -1,10 +1,15 @@
 import AnswerService from "../Services/AnswerService.js";
 import _store from "../store";
+import Answer from "../Models/Answer.js";
 
 //Private
 function _drawAnswer() {
+  let template = ''
   let answers = _store.State.answer;
+  answers.forEach((answer, i) => template = answer.answerTemplate(i))
   console.log(answers);
+
+  document.querySelector("#answer").innerHTML = template;
 }
 
 //Public
--- app/Controllers/QuestionController.js
+++ app/Controllers/QuestionController.js
@@ -3,8 +3,14 @@ import store from "../store";
 
 //Private
 function _drawQuestion() {
+  let template = ''
   let questions = store.State.question;
+  questions.forEach((question, i) => template = question.questionTemplate(i))
   console.log(questions);
+  console.log("Look ma, i drew a question");
+
+
+  document.querySelector("#question").innerHTML = template;
 }
 
 //Public
